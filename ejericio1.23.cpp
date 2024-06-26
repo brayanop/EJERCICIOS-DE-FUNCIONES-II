0a1280b9baaf79f29e4765d49f1b35075661c5a7
#SONY_FOX <131622618+brayanop@users.noreply.github.com>
2024-06-26 03:20:25 -0500

    Update ej2.cpp

--- ejericio1.23.cpp
+++ ejericio1.23.cpp
@@ -1,3 +1,4 @@
+//Crea un programa que calcule la hipotenusa de un triángulo rectángulo utilizando la fórmula de Pitágoras. Solicita al usuario los valores de los catetos y utiliza la función sqrt de cmath para calcular la raíz cuadrada de la suma de los cuadrados de los catetos. Muestra el resultado de la hipotenusa calculada.
 #include<bits/stdc++.h>
 #include"funciones.h"
 using namespace std;
